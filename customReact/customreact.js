a77ae50122abaf5e2b14a9a20b8eb3dc7dc4229e
neelam1424 <neelammore1404@gmail.com>
2025-12-07 11:29:33 -0500

    basics

---
+++ customReact/customreact.js
@@ -0,0 +1,39 @@
+
+function customRender(reactElement,container){
+
+//     const domElement = document.createElement(reactElement.type)
+// // put something inside it
+// domElement.innerHTML = reactElement.children
+// domElement.setAttribute('href', reactElement.props.href)
+// domElement.setAttribute('target', reactElement.props.target)
+// container.appendChild(domElement)
+
+const domElement = document.createElement(reactElement.type)
+
+domElement.innerHTML = reactElement.children
+for (const prop in reactElement.props) {
+    if(prop === 'children') continue
+    domElement.setAttribute(prop,reactElement.props[prop])
+}
+container.appendChild(domElement)
+
+}
+
+
+
+
+const reactElement={
+    type: "a",
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+
+    },
+    children:'Click me to visit google'
+}
+
+// connect to root
+const mainContainer=document.querySelector("#root")
+
+// method to inject what and where
+customRender(reactElement, mainContainer)
