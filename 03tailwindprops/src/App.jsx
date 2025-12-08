85ee276b842b7eb06d1bd7d2cfe33d310959c4b4
neelam1424 <neelammore1404@gmail.com>
2025-12-07 21:33:12 -0500

    Tailwind and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-3xl bg-green-500 p-3 rounded-md'>react course</h1>
+    <Card username="Neelam"/>
+    <Card/>
+    </> 
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Card({username = "Neelam"}) {
+    //console.log(props)
+  return ( 
+    
+    <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+  <div>
+    <img className="size-48 shadow-xl rounded-md" alt="" src="https://images.pexels.com/photos/33873305/pexels-photo-33873305.jpeg" />
+  </div>
+  <div className="flex items-center md:items-start">
+    <span className="text-2xl font-medium">{props.username }</span>
+    <span className="font-medium text-sky-500">The Anti-Patterns</span>
+    <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span> 
+  </div>
+ </div>
+
+  )
+}
+
+export default Card
